7704a9ec60253128631d13f17481e83a840e7a1c
Andy VanEe <1andyvanee@gmail.com>
2014-10-31 11:53:50 -0600

    db_url must be a string

--- lib/db.rb
+++ lib/db.rb
@@ -2,7 +2,7 @@ require 'sequel'
 
 if ENV['RACK_ENV'] == 'production'
   db_url = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/policy_uploader')
-  DB = Sequel.connect(db_url)
+  DB = Sequel.connect(db_url.to_s)
 elsif ENV['RACK_ENV'] == 'test'
   DB = Sequel.connect('sqlite://db/test.db')
 else
